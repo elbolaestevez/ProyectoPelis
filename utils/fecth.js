aaa1fd14a5f03c0af7b2e9d81048c8727e9fe2e4
Victor Balladares <victor.m.balladares.f@gmail.com>
2022-03-23 23:49:23 +0100

    Add all archives

--- utils/fecth.js
+++ utils/fecth.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 require('dotenv').config();
 const apiKey = process.env.API_KEY;
 
-const getProductByTitle = async (title) => {
+const getMovie = async (title) => {
     try {
         let response = await fetch(`http://www.omdbapi.com/?t=${title}&apikey=${apiKey}`); 
         let products = await response.json(); //{}
@@ -15,15 +15,7 @@ const getProductByTitle = async (title) => {
     }
 }
 
-
-const postFilm = (req, res) =>{
-    newFilm = req.body;
-    console.log(newFilm);
-    res.status(201).send(`Film ${newFilm.title} archivada`);
-}
-
 const movie = {
-    getProductByTitle,
-    postFilm
+    getMovie
 }
 module.exports=movie
